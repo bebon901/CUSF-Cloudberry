b502f261c10525f9e472e96ecb7cef0834218e0b
benbenson2004 <benbenson2004@gmail.com>
2023-02-14 11:44:30 +0000

    Changed code to loop till flight mode successfully obtained

--- put_gps_in_flight_mode.py
+++ put_gps_in_flight_mode.py
@@ -13,13 +13,14 @@ from pyubx2 import (
 
 # initialise global variables
 reading = False
-
+latest_reading = ""
+prev_read = ""
 def read_messages(stream, lock, ubxreader):
     """
     Reads, parses and prints out incoming UBX messages
     """
     # pylint: disable=unused-variable, broad-except
-
+    global latest_reading, prev_read
     while reading:
         if stream.in_waiting:
             try:
@@ -27,9 +28,11 @@ def read_messages(stream, lock, ubxreader):
                 (raw_data, parsed_data) = ubxreader.read()
                 lock.release()
                 if parsed_data:
-                    print(parsed_data)
+                    #print(parsed_data)
+                    prev_read = latest_reading
+                    latest_reading = parsed_data
             except Exception as err:
-                print(f"\n\nSomething went wrong {err}\n\n")
+                #print(f"\n\nSomething went wrong {err}\n\n")
                 continue
 
 
@@ -57,15 +60,17 @@ if __name__ == "__main__":
     port = "/dev/serial0"
     baudrate = 9600
     timeout = 0.1
-
+    model = 0
     with Serial(port, baudrate, timeout=timeout) as serial:
+      while model != 6:
+       try:
         msg = UBXMessage("CFG", "CFG-NAV5", SET, msgClass=0x06, msgID=0x24, fixMode=2, dyn=1, dynModel=0x06)
-        print(msg)
-        print(msg.serialize())
+        #print(msg)
+        #print(msg.serialize())
         # create UBXReader instance, reading only UBX messages
         ubr = UBXReader(BufferedReader(serial), protfilter=2)
 
-        print("\nStarting read thread...\n")
+        #print("\nStarting read thread...\n")
         reading = True
         serial_lock = Lock()
 
@@ -75,12 +80,8 @@ if __name__ == "__main__":
         msg = UBXMessage("CFG", "CFG-NAV5", POLL)
         send_message(serial, serial_lock, msg)
         sleep(1)
-        '''
-        print("\nPolling CFG configuration CFG-*...\n")
-        for (msgid, msgname) in UBX_MSGIDS.items():
-             if msgid[0] == 0x06:  # CFG-* configuration messages
-                 msg = UBXMessage("CFG", msgname, POLL)
-                 print(msgname, type(msgname))
-                 send_message(serial, serial_lock, msg)
-                 sleep(1.5)
-        '''
+        model = int(str(prev_read).split(",")[11].replace(" dynModel=", ""))
+        print("Model is:", str(model))
+       except:
+        print("Something Didn\'t work that time, I\'ll try again")
+      print("GPS Successfully in Flight Mode!!")
